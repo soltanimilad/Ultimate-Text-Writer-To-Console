7ee2a2a4b81afde17b6caedd3f71b9578a04ffe7
MiladSoltani <miladsoltani471@gmail.com>
2025-01-05 13:27:44 +0330

    Added icon

---
+++ Files/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ultimate_Text_Writer_To_Console
+{
+internal class Program
+        {
+            static void Main(string[] args)
+            {
+                ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+                string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()<>?||\\:;`~1234567890" + ' ';
+                string text = Console.ReadLine();
+
+                Random rand = new Random();
+                Console.Clear();
+                int i = 0;
+                while (i < text.Length)
+                {
+                    for (int j = 0; j < chars.Length && i < text.Length; j++)
+                    {
+                        Thread.Sleep(5);
+                        int Index = rand.Next(0, colors.Length);
+                        Console.ForegroundColor = colors[Index];
+                        if (text[i] == ' ')
+                        {
+                            Console.Write(text[i]);
+                            i++;
+                            continue;
+                        }
+                        Console.Write(chars[j]);
+                        if (chars[j] == text[i])
+                        {
+                            i++;
+                        }
+                        else
+                        {
+                            Console.Write("\b");
+                        }
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+}
